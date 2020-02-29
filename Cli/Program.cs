3c3b419533fa3474eca60f3d69efd26f0e15c42c
Rui Vilela <ruivil@gmail.com>
2020-02-29 00:24:33 +0000

    Trials

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -3,15 +3,14 @@
 namespace Cli
 {
     using Model;
+    using Model.CatalogCtx;
+
     class Program
     {
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            CatalogModel catalogModel = new CatalogModel();
-
+            CatalogModel catalogModel = new CatalogModel(new CatalogContext());
             catalogModel.CreateBook();
         }
     }
